e7216c119b9bb86b5499b98aa2e505eddaaf7fbe
françois QUENTIN <fquentin@epsi.fr>
2013-04-17 12:12:56 +0200

    Message de validation

---
+++ atelier 5/vieuModel/FilteredListViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace atelier_5.vieuModel
+{
+    public class FilteredListViewModel : INotifyPropertyChanged
+    {
+        private int _selectedFilter = 0;
+        private readonly string[] _filters;
+        private Model.EntrepriseEntities _context;
+
+        public FilteredListViewModel(Model.EntrepriseEntities context)
+        {
+            _context = context;
+            _filters = "Tout le staff,10$ et moins,par Date d'anni,par commande française, prix moyen par catégorie".Split(',');
+        }
+        public IEnumerable<object> FilteredList
+        {
+            get
+            {
+                switch (this._selectedFilter)
+                {
+                    case 0:
+                        return from employee in _context.Employees
+                               select new
+                               {
+                                   Prénom = employee.First_Name,
+                                   Nom = employee.Last_Name,
+                                   
+                               };
+                    case 1:
+                        return from product in _context.Products
+                               where product.Unit_Price < 10.0m
+                               select new
+                               {
+                                   Produit = product.Product_Name,
+                                   Prix = product.Unit_Price
+                               };
+
+                    case 2:
+                        return from employee in _context.Employees
+                               where employee.Birth_Date.HasValue == true
+                               where employee.Birth_Date.Value.Month== 01 
+                               select new
+                               {
+                                   Prénom = employee.First_Name,
+                                   Nom = employee.Last_Name
+                               };
+                    case 3:
+                        return from customer in _context.Customers
+                               where customer.customer_id == 
+                               where employee.Birth_Date.Value.Month == 01
+                               select new
+                               {
+                                   Prénom = employee.First_Name,
+                                   Nom = employee.Last_Name
+                               };
+                    case 4:
+                        return from employee in _context.Employees
+                               where employee.Birth_Date.HasValue == true
+                               where employee.Birth_Date.Value.Month == 01
+                               select new
+                               {
+                                   Prénom = employee.First_Name,
+                                   Nom = employee.Last_Name
+                               };
+                    
+                                   
+                                   default:
+                        return new string[] {
+"(Not implemented filter)"
+};
+                }
+            }
+        }
+        public IEnumerable<String> Filters
+        {
+            get { return _filters; }
+        }
+        public int SelectedFilter
+        {
+            get { return this._selectedFilter; }
+            set
+            {
+                this._selectedFilter = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this,
+                    new PropertyChangedEventArgs("FilteredList")
+                    );
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
